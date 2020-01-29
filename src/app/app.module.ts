ee67d296bb7f0ac1a1b932f714c177fa34d74c28
shhQuiet <stevehorne@gmail.com>
2020-01-28 18:10:10 -0600

    7 files modified, 2 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
 import { FoodSearchComponent } from "../food-search/food-search.component";
 import { MaterialModule } from "./material-module";
 
 @NgModule({
   imports: [BrowserModule, FormsModule, MaterialModule],
-  declarations: [AppComponent, HelloComponent, FoodSearchComponent],
+  declarations: [AppComponent, FoodSearchComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/food-search/food-search.component.ts
+++ src/food-search/food-search.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FoodService } from "../food.service";
 
 @Component({
-  selector: 'food-search',
-  templateUrl: './food-search.component.html',
-  styleUrls: ['./food-search.component.css']
+  selector: "food-search",
+  templateUrl: "food-search.component.html",
+  styleUrls: ["food-search.component.css"]
 })
 
 export class FoodSearchComponent implements OnInit {
+  constructor(private foodService: FoodService) {}
+  searchTerm = ''
+  ngOnInit() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  doSearch() {
+    console.log("Searching...");
+    this.foodService.find(this.searchTerm);
   }
-
 }
