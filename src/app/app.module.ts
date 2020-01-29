b2e6392be86be7cbe8c7a484edea3460bffd9890
shhQuiet <stevehorne@gmail.com>
2020-01-29 10:43:46 -0600

    2 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,36 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { FoodSearchComponent } from "../food-search/food-search.component";
+import { FoodDetailComponent } from "../food-detail/food-detail.component";
+import { HomeComponent } from "../home/home.component";
+import { PageNotFoundComponent } from "../page-not-found/page-not-found.component";
+
 import { MaterialModule } from "./material-module";
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "food-search", component: FoodSearchComponent },
+  { path: "food-detail/:fcid", component: FoodDetailComponent },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MaterialModule, HttpClientModule ],
-  declarations: [AppComponent, FoodSearchComponent],
+  imports: [
+    RouterModule.forRoot(routes, { enableTracing: true }),
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    HttpClientModule
+  ],
+  declarations: [
+    AppComponent,
+    FoodSearchComponent,
+    FoodDetailComponent,
+    HomeComponent,
+    PageNotFoundComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
